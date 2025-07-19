ef8d9c27678ece990517bed3c5c3713b4474bab8
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2025-07-19 13:37:22 +0300

    Enhance login tests and add data-test attributes for improved Cypress integration

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,11 @@
 describe('login testing', () => {
   it('login', () => {
     cy.visit('http://localhost:3000/')
-
+    cy.get('[data-test="inputHeader"]').should('contain', '<Anthony/>Recipe')
+    cy.get('[data-test="loginName"]').should('be.visible').type('Anthony')
+    cy.get('[data-test="loginPassword"]').should('be.visible').type('password123')
+    cy.get('[data-test="loginSbmt"]').click()
+    cy.url().should('include', '/home')
   })
+  
 })
